8d46737ed6f8ac0b683d62fa948d41d80d4a3e24
BennettDixon <bennettdixon16@gmail.com>
2024-11-12 20:51:55 +0000

    feat: wip tunnels

--- docker-public/ssh-tunnels/tunnel_manager.py
+++ docker-public/ssh-tunnels/tunnel_manager.py
@@ -2,6 +2,8 @@ import yaml
 import subprocess
 import time
 
+# TODO improve this, simple script for now
+
 def load_config():
     with open("./tunnels.yaml", "r") as file:
         return yaml.safe_load(file)
@@ -11,7 +13,8 @@ def establish_ssh_tunnel(ssh_host, ssh_user, local_port, remote_host, remote_por
         print(f"Forwarding {local_port} to {remote_host}:{remote_port} via {ssh_host}...")
         
         # Command to set up the SSH tunnel over Tailscale
-        command = f"ssh -f -N -L {local_port}:{remote_host}:{remote_port} {ssh_user}@{ssh_host}"
+        command = f"ssh -f -N -L {local_port}:{remote_host}:{remote_port} "
+                  f"{ssh_user}@{ssh_host} -o StrictHostKeyChecking=no"
         subprocess.run(command, shell=True, check=True)
         
         print(f"Tunnel established for {local_port} -> {remote_host}:{remote_port}")
